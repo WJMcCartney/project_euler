087c1509e245ce8c9a78b81e9e7a1c4a03673437
WJMcCartney <96458314+WJMcCartney@users.noreply.github.com>
2022-02-24 23:20:41 -0500

    finished problem one

---
+++ Project Euler/src/problemOne.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class problemOne {
+
+	public static void main(String[] args) {
+		int sum =0;
+		ArrayList<Integer> multiples = new ArrayList<Integer>();
+		for (int i=0; i<1000; i++) {
+			System.out.println(i);
+			if (i%3==0 || i%5==0) {
+				multiples.add(i);
+			}
+		}
+		for (int i=0; i<multiples.size();i++) {
+			sum += multiples.get(i);
+		}
+		System.out.println(multiples);
+		System.out.println(sum);
+	}
+
+}
