b23533ef352fda527d4342d179cf863d9211b0c0
WJMcCartney <96458314+WJMcCartney@users.noreply.github.com>
2022-02-24 23:21:37 -0500

    began project 2

---
+++ Project Euler/src/problemTwo.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class problemTwo {
+
+	public static void main(String[] args) {
+		int originalNumber = 1;
+		int additiveNumber = 1;
+		int nextNumber = 1;
+		int counter = 1;
+		int sum = 0;
+		ArrayList<Integer> evenFibNumbers = new ArrayList<Integer>();
+
+		
+		while (originalNumber < 400000001) {
+			if (originalNumber % 2 == 0) {
+				evenFibNumbers.add(originalNumber);
+			}
+			System.out.println(counter+". "+originalNumber);
+			nextNumber = originalNumber + additiveNumber;
+			additiveNumber = originalNumber;
+			originalNumber = nextNumber;
+			counter++;
+		}
+		System.out.println(evenFibNumbers);
+		for (int i=0; i<evenFibNumbers.size(); i++) {
+			sum += evenFibNumbers.get(i);
+			System.out.println(sum);
+		}
+
+	}
+
+}
