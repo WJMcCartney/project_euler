f5f1ec996d7532b67143dacec23011027417d15b
WJMcCartney <96458314+WJMcCartney@users.noreply.github.com>
2022-03-08 10:09:48 -0500

    forgot to save before committing last one

--- Project Euler/src/problemSix.java
+++ Project Euler/src/problemSix.java
@@ -1,19 +1,40 @@
+import java.util.*;
+import java.lang.reflect.Array;
 
 public class problemSix {
 
 	public static void main(String[] args) {
-		int sum=0;
-		int squareSum =0;
-		for (int i=0; i<=100; i++) {
-			sum += i;
-			squareSum = i*i + squareSum;
-			System.out.println(sum);
-			System.out.println(squareSum);
+		int none = 0;
+		int two = 0;
+		int three = 0;
+		int four = 0;
+		int five = 0;
+		int six = 0;
+		int n = 99999999;
+		int counter = 0;
+		int[] arrPS = { 0, 0, 0, 0, 0, 0 };
+		boolean prime[] = new boolean[n + 1];
+		for (int i = 0; i <= n; i++) {
+			prime[i] = true;
 		}
-		sum = sum * sum;
-		System.out.println(sum);
-		int finalAnswer = squareSum - sum;
-		System.out.println(finalAnswer);
+		for (int p = 2; p * p <= n; p++) {
+			if (prime[p] == true) {
+				for (int i = p * p; i <= n; i += p)
+					prime[i] = false;
+			}
+		}
+		for (int i = 2; i <= n; i++) {
+			if (prime[i] == true) {
+				counter++;
+				System.out.println(counter);
+				if (counter == 10001) {
+					System.out.println(i);
+					break;
+				}
+			}
+
+		}
+
 	}
 
 }
